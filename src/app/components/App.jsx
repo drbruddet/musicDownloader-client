6d67add67170afee3eea781578deda2c63a9002d
Guillaume Kolly <guillaume.kolly@gmail.com>
2017-05-07 15:37:29 +0200

    add a form and send to the server

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 export default class App extends React.Component {
 
-  componentDidMount() {
-    fetch('http://127.0.0.1:3000/').then(function(response) {
-      console.log(response);
-    }).catch(function(error) {
-      console.log('Request failed', error)
-    })
-  }
+    handleSubmit(event) {
+        event.preventDefault();
+        const url =  ReactDOM.findDOMNode(this.refs.urlInput).value.trim();
+        if (typeof window !== 'undefined' && url) {
+           window.location.href = "http://127.0.0.1:2000/" + "?url=" + url;
+        }
+        ReactDOM.findDOMNode(this.refs.urlInput).value = '';
+    }
 
-  render() {
-    return (
-     <div>
-       <h1>MAIN APP PAGE</h1>
-     </div>)
-  }
+    render() {
+        return (
+            <div>
+                <h1>MAIN APP PAGE</h1>
+                <form onSubmit={this.handleSubmit.bind(this)} >
+                    <input
+                        type="text"
+                        ref="urlInput"
+                        placeholder="Youtube Url ..."
+                    />
+                    <button type="submit" value="Submit">
+                        Download
+                    </button>
+                </form>
+            </div>
+        )
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,40 +5,40 @@ const APP_DIR = path.resolve(__dirname, 'src/app')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: APP_DIR + '/index.html',
-  filename: 'index.html',
-  inject: 'body'
+    template: APP_DIR + '/index.html',
+    filename: 'index.html',
+    inject: 'body'
 })
 
 const config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  devServer: {
-    inline: true,
-    port: 8080
-  },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx$/,
-        include : APP_DIR,
-        loader : 'babel-loader',
-        query: { presets: ['es2015', 'react'] },
-        exclude: /node_modules/
-      },
-      {
-        test : /\.js$/,
-        include : APP_DIR,
-        loader : 'babel-loader',
-        query: { presets: ['es2015', 'react'] },
-        exclude: /node_modules/
-      }
-    ]
-  },
-  plugins: [HtmlWebpackPluginConfig]
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+    module : {
+        loaders : [
+            {
+                test : /\.jsx$/,
+                include : APP_DIR,
+                loader : 'babel-loader',
+                query: { presets: ['es2015', 'react'] },
+                exclude: /node_modules/
+            },
+            {
+                test : /\.js$/,
+                include : APP_DIR,
+                loader : 'babel-loader',
+                query: { presets: ['es2015', 'react'] },
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
 };
 
 module.exports = config;
